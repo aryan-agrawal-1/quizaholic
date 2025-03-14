b4a23e1076b010fb90f8de050bfac16f5ec47825
“Firstname <2877689a@student.gla.ac.uk>
2025-03-14 17:21:04 +0000

    added profile view

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    created_by = models.ForeignKey(User, on_delete= models.CASCADE, related_name='created_by')
+
+class GameSession(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='game_of')
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='game_category')
+
+    MODE_CHOICES = [
+        ('basic', 'Basic'),
+        ('timed', 'Timed'),
+    ]
+
+    mode = models.CharField(max_length=50, choices=MODE_CHOICES)
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Question(models.Model):
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='for_category')
+    question_text = models.CharField(max_length=999)
+
+    DIFFICULTY_CHOICES = [
+        ('hard', 'Hard'),
+        ('medium', 'Medium'),
+        ('easy', 'Easy'),
+    ]
+
+    difficulty = models.CharField(max_length=30, choices=DIFFICULTY_CHOICES)
+    score = models.IntegerField()
+
+class Answer(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answer_of')
+    answer_text = models.CharField(max_length=999)
+    is_correct = models.BooleanField(default=True)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    streak = models.IntegerField()
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+
+def profile(request):
+    categories = Category.objects.filter(created_by = request.user)
+    return render(request, 'quiz/profile.html', {categories: categories})
+
