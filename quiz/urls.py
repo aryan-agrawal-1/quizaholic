fdca1259b93dfd2b9dffc9c35cf07b1f126b5449
Joani Baker <2762469b@student.gla.ac.uk>
2025-03-22 17:43:45 +0000

    alteringtemps

--- quiz/urls.py
+++ quiz/urls.py
@@ -9,12 +9,7 @@ urlpatterns = [
     path('categories/', views.categories, name = 'categories'),
     path('categories/<slug:category_name>/',views.category, name ='category' ),
     path('categories/<slug:category_name>/leaderboard', views.leaderboard, name = 'leaderboard'),
-    path('categories/<slug:category_name>/<str:mode>/question/<int:question_id>/', views.fetch_question, name = 'fetch_question')
-
-]
-
-
-urlpatterns = [
+    path('categories/<slug:category_name>/<str:mode>/question/<int:question_id>/', views.fetch_question, name = 'fetch_question'),
     path('profile/', views.profile, name='profile'),
     path('add_category/', views.add_category, name='add_category'),
     path('<slug:category_name_slug>/add_question', views.add_question, name='add_question'),
