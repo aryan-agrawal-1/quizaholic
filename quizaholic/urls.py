d39cc5d4e83ed032f5ce854dbbfe9ccd44195b2a
Joani Baker <2762469b@student.gla.ac.uk>
2025-03-22 17:23:23 +0000

    altered the quiz modes

--- quizaholic/urls.py
+++ quizaholic/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('admin/', admin.site.urls),
     path('quiz/', include('quiz.urls')),
+    path('admin/', admin.site.urls),
     
 ]
