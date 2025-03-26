1d1466b22302f8f720fb167c3dbfe8b7291927ed
Sara Abdel Bari <2722549a@student.ac.gla.uk>
2025-03-26 23:18:43 +0000

    updated forms

--- quiz/forms.py
+++ quiz/forms.py
@@ -1,16 +1,33 @@
 from django import forms
 from django.contrib.auth.models import User
 from quiz.models import UserProfile
+from django.contrib.auth.forms import UserCreationForm
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
     email = forms.EmailField(widget=forms.EmailInput())
+    
     class Meta:
         model = User
         fields = ('username', 'email', 'password',)
         exclude = ('category',)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = None 
+        
+
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('profile_picture',)
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = None
